8046182bf800babdd13ee8a3821c6b234a35bc64
Baylee Kimmel <bkimmel1226@gmail.com>
2025-04-25 00:11:42 -0400

    Imported csv data

--- src/libraries/data_ingestion.py
+++ src/libraries/data_ingestion.py
@@ -1,6 +1,14 @@
-import pandas as pd
-import numpy as np
+import csv
 
-def run():
-    df = pd.read_csv('../data/sales_data.csv')
-    print(df.head())
+with open('data/sales_data.csv', 'r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+
+    with open('data/cleaned_sales_data.csv', 'w', newline='') as new_file:
+        fieldnames = ['date', 'transaction_id', 'product_id', 'quantity', 'sale_amount']
+
+        csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames, delimiter='\t')
+
+        csv_writer.writeheader()
+
+        for row in csv_reader:
+            csv_writer.writerow(row)
