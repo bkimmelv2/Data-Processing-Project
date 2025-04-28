717ff06f46abba40b0fc88065fbd5c590dbef6bd
Baylee Kimmel <bkimmel1226@gmail.com>
2025-04-27 20:53:22 -0400

    Wrote functions in data_ingestion to read and normalize the sales data file and product data file.

--- src/libraries/data_ingestion.py
+++ src/libraries/data_ingestion.py
@@ -1,14 +1,45 @@
 import csv
+import json
+from datetime import datetime
 
-with open('data/sales_data.csv', 'r') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
+def read_sales_data(sales_filepath):
 
-    with open('data/cleaned_sales_data.csv', 'w', newline='') as new_file:
-        fieldnames = ['date', 'transaction_id', 'product_id', 'quantity', 'sale_amount']
+    # Initialize an empty list to store the sales data
+    sales_data = []
 
-        csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames, delimiter='\t')
+    # Read the sales data from the CSV file
+    with open(sales_filepath, mode='r', newline='') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            # Retrieve the date from the current row
+            date = row['date']
 
-        csv_writer.writeheader()
+            # First try to parse the date as a YYYY-MM-DD format
+            try:
+                parsed_date = datetime.strptime(date, '%Y-%m-%d')
+            # If that fails, try to parse it as a date in MM/DD/YYYY format
+            except ValueError:
+                parsed_date = datetime.strptime(date, '%m/%d/%Y')
+            
+            # Convert the date back into a string in YYYY-MM-DD format when storing the data back in the row
+            row['date'] = parsed_date.strftime('%Y-%m-%d')
+            
+            # Convert quantity and sale_amount to appropriate data types
+            row['quantity'] = int(row['quantity'])
+            row['sale_amount'] = float(row['sale_amount'])
+            
+            sales_data.append(row)
+    print(sales_data)
+    return sales_data
 
-        for row in csv_reader:
-            csv_writer.writerow(row)
+def read_product_data(product_filepath):
+    # Read the product data from the JSON file
+    with open(product_filepath) as file:
+        # Load the JSON data into a Python dictionary
+        product_data = json.load(file)
+    # Create a dictionary indexed by product_id for easy lookup
+    print({product['product_id']: product for product in product_data})
+    return {product['product_id']: product for product in product_data}
+
+# read_sales_data('../data/sales_data.csv')
+# read_product_data('../data/product_data.json')
--- src/scripts/process_data.py
+++
@@ -1,3 +0,0 @@
-import libraries.data_ingestion as data_ingestion
-
-data_ingestion.run()
